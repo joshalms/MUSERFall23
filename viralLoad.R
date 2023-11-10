4200ec03e601fab2f427581090cbb91ad40e83f6
alaynabinder <ajb197@duke.edu>
2023-11-10 01:02:04 -0500

    viralload attempt

--- viralLoad.R
+++ viralLoad.R
@@ -1,9 +1,32 @@
+
 #Approach:
   #Increment viral load for all E and I (should be very short)
 
 
 
 viral_load <- function(dat, at) {
+
+  ## Attributes ##
+
+  active <- get_attr(dat, "active") #list of all people who are doing something at this time step
+  status <- get_attr(dat, "status") #List of all statuses with indices corresponding to IDs
+
+  infTime <- get_attr(dat, "infTime") # Use infTime to find viral load in aerosol
+
+
+  for (id in idsAgents) {
+    if (timestep = 1) { # pseudocode
+      viraLoad <- sample(possible_viral_loads, size = 1, replace = FALSE) # Pull viral load from distribution, TBD?
+      dat$attributes$viralLoad[dat$agents$id == person] <- viralLoad # Assign the viral load to the person
+    } else {
+      if (dat$status[dat$id == id] %in% c("e", "i")) {
+        dat$attributes$viralLoad[dat$agents$id == person] <- viralLoad + increment # What to increment by?
+      }
+    }
+  }
+
+  infectID <- cbind(active, status, infTime, viralLoad) #Just puts these attribute columns together
+
 
   return(dat)
 }
