912f0ec25abdf73b2101be8116d9e88475f4c3bb
alaynabinder <ajb197@duke.edu>
2023-11-09 21:22:35 -0500

    Comments for makeRooms

--- makeRooms.R
+++ makeRooms.R
@@ -1,14 +1,14 @@
 assignRooms <- function(idsAgents) {
 
-  nRooms = 10
+  nRooms = 10 # Number of rooms
 
-  Rooms <- vector("list", length = nRooms)
+  Rooms <- vector("list", length = nRooms) # Creates list of specified number of rooms
 
-  for (id in idsAgents) {
-    idx <- sample(1:nRooms, 1)
-    Rooms[[idx]] <- c(Rooms[[idx]], id)
+  for (id in idsAgents) { # Iterate through each person using their ID
+    idx <- sample(1:nRooms, 1) # Chooses a random room to add person to
+    Rooms[[idx]] <- c(Rooms[[idx]], id) # Adds that person's ID to the room
   }
 
-  return(Rooms)
+  return(Rooms) # Returns updated list of rooms. Each room is represented as a list containing the IDs of the people assigned to that room
 
 }
